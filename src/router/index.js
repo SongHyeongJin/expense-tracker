2e1d6510defe3de0e382a3042e3ec16a1a42ad6b
doherun <doherun@gmail.com>
2026-04-10 15:27:53 +0900

    feat: code-fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
+﻿import { createRouter, createWebHistory } from 'vue-router';
 import TransactionFormPage from '@/pages/TransactionFormPage.vue';
 import TransactionsPage from '@/pages/TransactionsPage.vue';
 import SummaryPage from '@/pages/SummaryPage.vue';
-import HomePage from '@/pages/HomePage.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomePage },
+    {
+      path: '/',
+      redirect: '/transactions',
+    },
     {
       path: '/transactions',
       name: 'transactions',
